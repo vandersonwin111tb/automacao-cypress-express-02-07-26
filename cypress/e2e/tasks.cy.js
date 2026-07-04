44ae6da971e44c0800e10926a9c60a07f73be26b
vandersonwin111tb <vandersonwin111tb@gmail.com>
2026-07-04 11:19:37 -0300

    testes de ponta a ponta - validando estilos de CSS

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -1,49 +1,66 @@
 /// <reference types="cypress" />
 
 describe('tasks', () => {
-    it('deve cadastrar uma nova tarefa', () => {
+    context('cadastro', () => {
+        it('deve cadastrar uma nova tarefa', () => {
 
-        var taskName = 'Ler um livro de Node.js'
+            var taskName = 'Ler um livro de Node.js'
 
-        cy.removeTaskByName(taskName)
+            cy.removeTaskByName(taskName)
 
-        cy.createTask(taskName)
+            cy.createTask(taskName)
 
-        // cy.get('main div p')
-        //     .should('be.visible')
-        //     .should('have.text', 'Ler um livro de node.js')
+            // cy.get('main div p')
+            //     .should('be.visible')
+            //     .should('have.text', 'Ler um livro de node.js')
 
-        cy.contains('main div p', taskName)
-            .should('be.visible')
+            cy.contains('main div p', taskName)
+                .should('be.visible')
 
+        })
+
+        it('Não deve permitir tarefa duplicada', () => {
+
+            const task = {
+                name: 'Estudar Javascript',
+                is_done: false
+            }
+
+            cy.removeTaskByName(task.name)
+            cy.postTask(task)
+
+            // Dado que eu tenho uma tarefa duplicada
+            cy.createTask(task.name)
+
+            // Quando faço o cadastro dessa tarefa
+            cy.createTask(task.name)
+
+            // Então veja a mensagem de duplicidade
+            cy.get('.swal2-html-container')
+                .should('be.visible')
+                .should('have.text', 'Task already exists!')
+        })
+
+        it('Campo obrigatório', () => {
+            cy.createTask()
+
+            cy.isRequired('This is a required field')
+        })
     })
+    context('atualização', ()=> {
+        it('Deve concluir uma tarefa', () => {
+            const taskName = 'Pagar contas de consumo'
 
-    it('Não deve permitir tarefa duplicada', () => {
+            cy.visit('http://localhost:3000')
 
-        const task = {
-            name: 'Estudar Javascript',
-            is_done: false
-        }
+            cy.contains('p', taskName)
+                .parent()
+                .find('button[class*=ItemToggle]')
+                .click()
 
-        cy.removeTaskByName(task.name)
-        cy.postTask(task)
-
-        // Dado que eu tenho uma tarefa duplicada
-        cy.createTask(task.name)
-
-        // Quando faço o cadastro dessa tarefa
-        cy.createTask(task.name)
-
-        // Então veja a mensagem de duplicidade
-        cy.get('.swal2-html-container')
-            .should('be.visible')
-            .should('have.text', 'Task already exists!')
-    })
-
-    it('Campo obrigatório', () => {
-        cy.createTask()
-
-        cy.isRequired('This is a required field')
+            cy.contains('p', taskName)
+                .should('have.css', 'text-decoration-line', 'line-through')
+        })
     })
 })
 
