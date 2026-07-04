66ca335f6d115964f466e3145f8d16a7a32a4e76
vandersonwin111tb <vandersonwin111tb@gmail.com>
2026-07-04 06:35:37 -0300

    dados dinamicos com faker

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -1,11 +1,13 @@
 /// <reference types="cypress" />
 
+import {faker} from '@faker-js/faker'
+
 describe('tasks', () => {
     it('deve cadastrar uma nova tarefa', ()=> {
         cy.visit('http://localhost:3000')
 
         cy.get('input[placeholder="Add a new Task"]')
-            .type('Ler um livro de node.js')
+            .type(faker.git.branch()) // 'feed-parse' 
 
         // cy.get('._listButtonNewTask_1y0mp_40')
         cy.contains('button', 'Create').click()
