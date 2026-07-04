a58370a5c1e392dee9382694ee7903e57b0a8dbb
vandersonwin111tb <vandersonwin111tb@gmail.com>
2026-07-04 09:44:21 -0300

    testes de ponta a ponta - testes independentes

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -26,4 +26,43 @@ describe('tasks', () => {
             .should('be.visible')
 
     })
+
+    it('Não deve permitir tarefa duplicada', ()=> {
+
+        cy.request({
+            url: 'http://localhost:3333/helper/tasks',
+            method: 'DELETE',
+            body: { name: 'Estudar Javascript'}
+        }).then(response => {
+            expect(response.status).to.eq(204)
+        })
+
+        // Dado que eu tenho uma tarefa duplicada
+        cy.request({
+            url: 'http://localhost:3333/tasks',
+            method: 'POST',
+            body: { name: 'Estudar Javascript', is_done: false }
+        }).then(response => {
+            expect(response.status).to.eq(201)
+        })
+        // Quando faço o cadastro dessa tarefa
+        cy.visit('http://localhost:3000')
+
+        cy.get('input[placeholder="Add a new Task"]')
+            .type('Estudar Javascript')
+
+        cy.contains('button', 'Create').click()
+
+        cy.visit('http://localhost:3000')
+
+        cy.get('input[placeholder="Add a new Task"]')
+            .type('Estudar Javascript')
+
+        cy.contains('button', 'Create').click()
+
+        // Então veja a mensagem de duplicidade
+        cy.get('.swal2-html-container')
+            .should('be.visible')
+            .should('have.text', 'Task already exists!')
+    })
 })
