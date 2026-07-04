584e31e0bffb3fafa798425ac7ddc6c5da1e95cf
vandersonwin111tb <vandersonwin111tb@gmail.com>
2026-07-04 07:20:10 -0300

    testes de ponta a ponta - consumindo a API Rest

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -1,15 +1,21 @@
 /// <reference types="cypress" />
 
-import {faker} from '@faker-js/faker'
-
 describe('tasks', () => {
-    it('deve cadastrar uma nova tarefa', ()=> {
+    it('deve cadastrar uma nova tarefa', () => {
+
+        cy.request({
+            url: 'http://localhost:3333/helper/tasks',
+            method: 'DELETE',
+            body: { name: 'Ler um livro de node.js' }
+        }).then(response => {
+            expect(response.status).to.eq(204)
+        })
+
         cy.visit('http://localhost:3000')
 
         cy.get('input[placeholder="Add a new Task"]')
-            .type(faker.git.branch()) // 'feed-parse' 
+            .type('Ler um livro de node.js')
 
-        // cy.get('._listButtonNewTask_1y0mp_40')
         cy.contains('button', 'Create').click()
 
     })
