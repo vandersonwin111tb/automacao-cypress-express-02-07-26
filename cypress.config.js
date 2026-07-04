d31192431cbfd7f0a33f6df4fa1d41740ff5e0d6
vandersonwin111tb <vandersonwin111tb@gmail.com>
2026-07-04 12:28:14 -0300

    testes de ponta a ponta - viewport size

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,8 @@ module.exports = {
     env: {
       apiUrl: 'http://localhost:3333'
     },
+    viewportWidth: 1920,
+    viewportHeight: 1080,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
